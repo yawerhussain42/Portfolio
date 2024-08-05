9b9ebcfae2d73f8e6fc6673b4572abd44d9ce051
Yawar Hussain <yawar.hussain@getafixtechnologies.com>
2024-08-06 02:22:43 +0530

    added base path for images

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -11,7 +11,7 @@ export const DATA = {
     "Senior Frontend Engineer | Expert in React, Angular, JavaScript, TypeScript, ElectronJS| Proven Team Leader | Web Development Specialist",
   summary:
     "I specialize in developing high-performing, scalable user interfaces with modern JavaScript frameworks like React. Driven by a passion for crafting seamless digital experiences, I collaborate closely with cross-functional teams to deliver robust, user-centric designs. Leveraging Agile methodologies, I ensure timely project completions while maintaining high code quality. As a mentor and leader, I conduct rigorous code reviews and foster a culture of continuous improvement, focusing on creating solutions that are both testable and scalable. My goal is to push the boundaries of technology and user experience, delivering innovative solutions that delight users and drive business success.",
-  avatarUrl: "/me.jpg",
+  avatarUrl: "Portfolio/me.jpg",
   skills: [
     "React",
     "Javascript",
